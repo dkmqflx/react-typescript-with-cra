0de13eea06e2ee1b8b1d707e424841c7f4332244
dkmqflx <dkmqflx@gmail.com>
2022-04-14 16:09:27 +0900

    apply style-reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
+
 function App() {
-  return <div>React TypeScript with CRA</div>;
+  return (
+    <div>
+      <GlobalStyle></GlobalStyle>
+      <h1> React TypeScript with CRA</h1>
+      <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
